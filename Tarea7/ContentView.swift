40eec06c504b048095db39ef521378a961903036
Daniel Cubillo <dcubillo81@gmail.com>
2021-03-25 09:23:23 -0600

    Initial Commit

---
+++ Tarea7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tarea7
+//
+//  Created by Daniel Cubillo on 25/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tarea7/Tarea7App.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea7App.swift
+//  Tarea7
+//
+//  Created by Daniel Cubillo on 25/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Tarea7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
